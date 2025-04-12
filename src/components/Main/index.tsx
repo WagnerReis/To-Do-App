76c2822db4ed2aa8a5ee59852ff7f8b112cbab2e
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-12 16:22:44 -0300

    feat(Main): add tasks info section

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -9,6 +9,13 @@ export function Main() {
         <input type="text" placeholder="Adicione uma nova tarefa" />
         <button type="submit">Criar <PlusCircle size={16} weight="bold"/></button>
       </div>
+
+      <div className={styles.tasksContainer}>
+        <div className={styles.tasksInfo}>
+          <p className={styles.createds}>Tarefas criadas <span>0</span></p>
+          <p className={styles.completeds}>Concluídas <span>0 de 5</span></p>
+        </div>
+      </div>
     </main>
   )
 }
